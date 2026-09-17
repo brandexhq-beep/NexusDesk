eef2aff1db98a43077b248c671aa7623cf9e7b9a
brandexhq-beep <brandexhq@gmail.com>
2026-09-17 17:46:03 +0530

    feat: add service worker for asset precaching and routing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.96u38o7sgh"
+    "revision": "0.odtf0u175kk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
